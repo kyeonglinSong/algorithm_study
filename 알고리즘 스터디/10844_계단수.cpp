54ae3d15dc1f210e231048d6a9aa3a6abe0eff44
kyeonglin_Song <42909525+kyeonglinSong@users.noreply.github.com>
2019-10-14 14:03:44 +0900

    191014 dp

---
+++ 알고리즘 스터디/10844_계단수.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+const int MOD = 1000000000;
+int n;
+int cache[10][101];
+
+int dp(int digit, int length)
+{
+	if (digit < 0 || digit>9)
+		return 0;
+
+	int& result = cache[digit][length];
+	if (result != -1)
+		return result;
+	return result = (dp(digit - 1, length - 1) + dp(digit + 1, length - 1)) % MOD;
+}
+
+int main()
+{
+	cin >> n;
+	memset(cache, -1, sizeof(cache));
+	for (int i = 0; i < 10; i++)
+		cache[i][1] = 1;
+
+	int sum = 0;
+	for (int i = 1; i <= 9; i++)
+	{
+		sum += dp(i, n);
+		sum %= MOD;
+	}
+	cout << sum << endl;
+	return 0;
+}
