fc0938813e6dbd8a0e4a05bf40cb472f8b83c0d8
kyeonglin_Song <blossommilktea@gmail.com>
2020-04-18 16:27:58 +0900

    [200418] 어제 문제 upgrade

--- C++/11053.cpp
+++ C++/11053.cpp
@@ -1,34 +1,71 @@
+// 최장 증가 부분수열(이하 lis)의 길이와 수열을 모두 출력하는 코드.
+
 #include <iostream>
 using namespace std;
 
-const int MAX = 1001;
+const int MAX = 1001; // MAX = 최대 입력 길이 + 1
 int num; // 입력 배열의 원소 갯수
 int arr[MAX]; // 입력 배열
 
-int longest_increasing_subseq()
+// lis와 lis수열을 구하고, 결과들을 출력하는 함수
+void longest_increasing_subseq()
 {
-    int h[MAX] = {0}; // i로 끝나는 최장 증가 부분수열의 길이 배열
-    int max = 0; // 현재 max
+    int h[MAX] = {0}; // arr[i]로 끝나는 lis의 길이 배열 
+    int past[MAX] = {0}; // arr[i]의 바로 전 값의 인덱스 배열
 
-    for(int i=1 ; i<=num ; i++) // arr[i]에서의 최장 증가 부분수열의 길이를 구한다
+    // arr[i]에서의 lis의 길이를 h배열에 저장한다.
+    for(int i=1 ; i<=num ; i++) 
         for(int j=0 ; j<i ; j++)
             if (arr[i] > arr[j] && h[i] < h[j]+1) 
+            {
+                // h[i]에 이전 lis의 길이 + 1(자신)을 저장한다.
                 h[i] = h[j]+1;
+                // past[i]에 이전 lis의 제일 마지막 인덱스를 저장한다.
+                past[i] = j;
+            }
 
-    for(int i=1 ; i<=num ; i++) // h[i]중 최댓값을 구한다
-        if(max < h[i])
-            max = h[i];
+    int result = 0; // 반환할 값. lis의 길이
+    int index; // result의 인덱스
+    
+    // h[i]중 최댓값과 그 인덱스를 구한다.
+    for(int i=1 ; i<=num ; i++) 
+        if(result < h[i])
+        {
+            result = h[i];
+            index = i; 
+        }
+    
+    int lis[MAX]; // lis 수열을 저잘할 배열
+    int temp = result; // lis 값들의 저장을 위한 임시 인덱스
 
-    return max; // 최댓값을 리턴한다
+    while(index != 0)
+    {
+        // arr[index]에 해당하는 값을 lis배열에 저장한다.
+        lis[--temp] = arr[index]; 
+        // 인덱스를 이전 값의 인덱스로 변경한다.
+        index = past[index];
+    }
+
+    // lis의 길이, 수열 출력 부분
+    cout << "최장 증가 부분 수열의 길이 : " << result << "\n";
+    cout << "최장 증가 부분 수열 : [ ";
+    for(int i=0 ; i<result ; i++)
+        cout << lis[i] << " ";
+    cout << "]";
+
 }
 
+// 메인 함수
 int main()
 {
+    // 전체수열의 길이와 수열을 입력받는다.
+    // 입력 크기와 배열은 모두 전역변수로 선언했다.
     cin >> num;
     for (int i=1 ; i<=num ; i++)
         cin >> arr[i];
 
-    cout << longest_increasing_subseq();
+    // lis와 그 길이를 구하고 출력한다.
+    longest_increasing_subseq();
 
     return 0;
 }
--- C++/1932.cpp
+++ C++/1932.cpp
@@ -1,36 +1,83 @@
+// 삼각형 합과 경로에 있는 수 출력
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-const int MAX = 500;
-int num;
-int arr[MAX][MAX];
+const int MAX = 501; // MAX : 최대 입력 길이 + 1
+int num; // num : number, 입력받을 삼각형의 높이이자 밑변의 원소 수
+int triangle[MAX][MAX]; // triangle : 입력받을 삼각형
 
-int number_triangle()
+// 삼각형의 최대 합 경로와 최대 합을 구하고 출력하는 함수
+void number_triangle()
 {
-    int best[MAX][MAX] = {0};
-    int result = 0;
+    int best[MAX][MAX] = {0}; // best : 각 원소까지의 최대 합을 저장한 배열
+    // direction : 해당 원소까지의 최대경로상의 바로 윗 level 값의 방향.
+    // 초기값 -1, 왼쪽 0, 오른쪽 1
+    int direction[MAX][MAX] = {-1}; 
 
-    best[0][0] = arr[0][0]; // 초기조건, 삼각형의 맨 꼭대기값은 그대로.
-    for(int i=1 ; i<=num ; i++)
+    // 초기조건, 삼각형의 맨 꼭대기값의 최대합은 값 자체이다.
+    best[0][0] = triangle[0][0]; 
+
+    // 삼각형의 각 원소에서의 최대 합과 그 이전 level 원소의 방향을 저장한다.
+    for(int i=1 ; i<num ; i++)
         for(int j=0 ; j<=i ; j++)
-            best[i][j] = arr[i][j] + max(best[i-1][j-1], best[i-1][j]);
-    
+        {
+            best[i][j] = triangle[i][j] + max(best[i-1][j-1], best[i-1][j]);
+            // 왼쪽 윗 값에서 내려왔다면, direction을 왼쪽으로 설정
+            if (best[i-1][j-1] > best[i-1][j]) 
+                direction[i][j] = 0; 
+            // 오른쪽 윗 값에서 내려왔다면, direction을 오른쪽으로 설정
+            else
+                direction[i][j] = 1; 
+        }
+
+    int result = 0; // result : 최대 합.
+    int index; // index : 최대 합의 열(column)에서의 위치 인덱스
+
+    // 맨 아래 level에서 삼각형의 최대 합과 그 열 인덱스를 구한다.
     for(int j=0 ; j<num ; j++)
-        if(result < best[num][j])
-            result = best[num][j];
+        if(result < best[num-1][j])
+        {
+            result = best[num-1][j];
+            index = j;
+        }
+
+    // route : max sum route. 최대 합의 경로 원소를 저장한 배열
+    int route[MAX] = {0};
+
+    // 가장 밑 level에서부터 direction배열을 통해 경로를 역추적하며 최대 합 경로를 저장한다.
+    for(int i=num-1 ; i>=0 ; i--)
+    {
+        route[i] = triangle[i][index];
+        // 이전 경로 방향이 왼쪽이라면 index를 왼쪽으로 이동시킨다.
+        // 열 인덱스에서 왼쪽 : n-1, 오른쪽 : n 그대로임을 이용.
+        if (direction[i][index] == 0) 
+            index = index-1;
+    }
+
+    // 삼각형의 최대 합과 그때의 경로를 출력한다.    
+    cout << "최대 합 : " << result << "\n";
+    cout << "최대 합일때의 경로 : [ ";
+    for(int i=0 ; i<num ; i++)
+        cout << route[i] << " ";
+    cout << "]";
     
-    return result;
 }
 
+// 메인 함수
 int main()
 {
+    // 삼각형의 높이와 삼각형을 구성하는 원소들을 입력받는다.
+    // 높이와 원소 배열은 모두 전역변수로 선언하였다. 
     cin >> num;
     for (int i=0 ; i<num ; i++)
         for(int j=0 ; j<=i ; j++)
-            cin >> arr[i][j];
+            cin >> triangle[i][j];
     
-    cout << number_triangle();
+    // 삼각형의 최대 합 경로와 최대 합을 구하고, 출력한다.
+    // 출력되는 경로는 삼각형 꼭대기에서부터 아래쪽을 향하는 경로이다.
+    number_triangle();
 
     return 0;
 }
